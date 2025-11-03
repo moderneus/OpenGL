db5fba6ede792ba6fde3161144c128a226dd3c5b
moderneus <finelefrinchik@gmail.com>
2025-11-03 16:02:18 +0400

    Window creation

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,5 @@
+#include <SDL3/SDL.h>
+
 #include <fmt/core.h>
 #include <fmt/base.h>
 #include <fmt/color.h>
@@ -6,7 +8,36 @@
 
 int main()
 {
-    fmt::print(fmt::fg(fmt::color::sea_green), "Hello, OpenGL!\n");
+    if(SDL_Init(SDL_INIT_VIDEO) != true)
+    {
+        fmt::print(fmt::fg(fmt::color::red), "Error: SDL3 initialization failed: ");
+        fmt::print(fmt::fg(fmt::color::white_smoke), "{}\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    
+    SDL_Window* pWindow = SDL_CreateWindow("OpenGL", 800, 600, SDL_WINDOW_OPENGL);
+
+    if(pWindow == NULL)
+    {
+        fmt::print(fmt::fg(fmt::color::red), "Error: Window creation failed: ");
+        fmt::print(fmt::fg(fmt::color::white_smoke), "{}\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    
+    SDL_Event event;
+    
+    bool quit = false;
+    while(!quit)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_EVENT_QUIT)
+                quit = true;
+        }
+    }
+
+    SDL_DestroyWindow(pWindow);
+    SDL_Quit();
     
     return EXIT_SUCCESS;
 }
